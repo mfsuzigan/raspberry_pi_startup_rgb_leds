6131e1bc9df0532446f840e2dbe235a4d0efe404
Michel Suzigan <michel.suzigan@tqi.com.br>
2020-01-19 23:50:34 -0200

    Ajuste no tempo de sinal

--- startup_leds.py
+++ startup_leds.py
@@ -9,13 +9,16 @@ pixel_pin = board.D18
 # Numero de leds que serao controlados
 num_pixels = 16
 
+# Tempo do sinal em segundos
+signal_time = 2
+
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, 
     brightness=1, auto_write=False,
     pixel_order=neopixel.GRB)
 
 pixels.fill((255, 255, 255))
 pixels.show()
-time.sleep(3)
+time.sleep(signal_time)
 pixels.fill((0,0,0))
 pixels.show()
 GPIO.cleanup()
